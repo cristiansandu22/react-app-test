5c3cc79c45829b5066772d9c0136410fb6c9eee1
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-08 22:10:20 +0200

    Use constructor and state

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import './App.css'
 import {Component} from "react";
 
 class App extends Component{
-    // const [count, setCount] = useState(0)
-    //
-    // const [name, setName] = useState('Chris');
 
+constructor() {
+    super();
+
+    this.state = {
+        name: 'Chris'
+    }
+}
 
 render() {
+
+
     return (
         <>
             <h1>Test</h1>
             <div className="card">
-                <button>
-                    count is ...
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
+                <p>My name is {this.state.name} </p>
+                <button onClick={() => this.state.name = 'Emilique'}>Change name</button>
+                <button onClick={() => console.log(this.state.name)}>ConsoleLOG</button>
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
