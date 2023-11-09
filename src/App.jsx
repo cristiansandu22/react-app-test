4335d93bc546132e49c16147aec2a808f4f11168
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-09 19:38:52 +0200

    API call
    API call and populating the monsters array instead of hardcoding it.

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,52 @@ class App extends Component{
         super();
 
         this.state = {
-            monsters: [
-                {
-                    id: 1,
-                    name: 'Cristinel'
-                },
-                {
-                    id: 2,
-                    name: 'Emilique'
-                },
-                {
-                    id: 3,
-                    name: 'Madalinutz'
-                },
-                {
-                    id: 4,
-                    name: 'Didilique'
-                }
-            ]
+            monsters: []
         }
+    }
+
+    async componentDidMount() {
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.json())
+        //     .then(data => this.setState(() => {
+        //             return {monsters: data}
+        //         }, () => {
+        //             console.log(this.state)
+        //         }
+        //     ))
+
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+            if(!response.ok) {
+                throw new Error('Network response was not ok!')
+            }
+
+            const users = await response.json()
+            this.setState(() => {
+                return {monsters: users}
+            }, () => {
+                console.log(this.state)
+            });
+
+        } catch (err) {
+            console.error(`Error fetching data: ${err}`)
+        }
+
+        // const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        // const users = await response.json()
+        //
+        // this.setState({monsters: users}, () => {
+        //     console.log(this.state)
+        // })
+
+        // const hello = async function() {
+        //     const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        //     const users = await data.json()
+        //     return this.setState({monsters: users})
+        // }
+        //
+        // hello()
     }
 
     render() {
