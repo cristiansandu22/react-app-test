4f57078aa12a09804996d7af69f115549202765e
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-17 18:02:28 +0200

    Card component created

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,15 @@
 import {Component} from "react";
+import './card-list.styles.css'
 
 class CardList extends Component {
     render() {
         const {monsters} = this.props
 
         return (
-            <div>
+            <div className='grid'>
                 {monsters.map(monster => {
                     return (
-                        <div key={monster.id}>
+                        <div key={monster.id} className='container'>
                             <h3>{monster.name}</h3>
                         </div>
                     )
