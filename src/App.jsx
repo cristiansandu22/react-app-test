a2427197557f94deb95b72e778cc8c68a1c9010b
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-20 11:58:26 +0200

    Transform App
    Transform App into a functional component.
    
    Starting to change the application. We will use functional components instead of class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,108 @@
 import './App.css'
-import {Component} from "react";
+import {useState} from "react";
 import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/search-box.component.jsx";
 
-class App extends Component{
 
-    constructor() {
-        super();
+const App = () => {
 
-        this.state = {
-            monsters: [],
-            searchedField: '',
-        }
+    console.log('render');
+
+    const [searchField, setSearchField] = useState('')
+
+    const onChangeField = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase()
+        setSearchField(searchFieldString)
     }
 
-    async componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(res => res.json())
-        //     .then(data => this.setState(() => {
-        //             return {monsters: data}
-        //         }, () => {
-        //             console.log(this.state)
-        //         }
-        //     ))
+    console.log(searchField)
 
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    return(
+        <>
+            <h1>Hello</h1>
+            <SearchBox
+                onChangeHandler={onChangeField}
+                inputPlaceholder='Search monsters'
+                inputClassName='search-box'
+            />
 
-            if(!response.ok) {
-                throw new Error('Network response was not ok!')
-            }
-
-            const users = await response.json()
-            this.setState(() => {
-                return {monsters: users}
-            });
-
-        } catch (err) {
-            console.error(`Error fetching data: ${err}`)
-        }
-    }
-
-    onChangeField = (event) => {
-        const monsterInputValue = event.target.value.toLocaleLowerCase()
-
-        this.setState(() => {
-            return {
-                searchedField: monsterInputValue
-            }
-        })
-
-    }
-
-    render() {
-
-        const {monsters, searchedField} = this.state;
-        const {onChangeField} = this;
-
-        const filteredMonsters = monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchedField)
-        })
-
-
-        return (
-            <>
-
-                <SearchBox
-                    onChangeHandler={onChangeField}
-                    inputPlaceholder='Search monsters'
-                    inputClassName='search-box'
-                />
-
-                <CardList monsters={filteredMonsters}/>
-            </>
-        )
-    }
+            {/*<CardList monsters={filteredMonsters}/>*/}
+        </>
+    )
 }
+
+// class App extends Component{
+//
+//     constructor() {
+//         super();
+//
+//         this.state = {
+//             monsters: [],
+//             searchedField: '',
+//         }
+//     }
+//
+//     async componentDidMount() {
+//         // fetch('https://jsonplaceholder.typicode.com/users')
+//         //     .then(res => res.json())
+//         //     .then(data => this.setState(() => {
+//         //             return {monsters: data}
+//         //         }, () => {
+//         //             console.log(this.state)
+//         //         }
+//         //     ))
+//
+//         try {
+//             const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//
+//             if(!response.ok) {
+//                 throw new Error('Network response was not ok!')
+//             }
+//
+//             const users = await response.json()
+//             this.setState(() => {
+//                 return {monsters: users}
+//             });
+//
+//         } catch (err) {
+//             console.error(`Error fetching data: ${err}`)
+//         }
+//     }
+//
+//     onChangeField = (event) => {
+//         const monsterInputValue = event.target.value.toLocaleLowerCase()
+//
+//         this.setState(() => {
+//             return {
+//                 searchedField: monsterInputValue
+//             }
+//         })
+//
+//     }
+//
+//     render() {
+//
+//         const {monsters, searchedField} = this.state;
+//         const {onChangeField} = this;
+//
+//         const filteredMonsters = monsters.filter((monster) => {
+//             return monster.name.toLocaleLowerCase().includes(searchedField)
+//         })
+//
+//
+//         return (
+//             <>
+//
+//                 <SearchBox
+//                     onChangeHandler={onChangeField}
+//                     inputPlaceholder='Search monsters'
+//                     inputClassName='search-box'
+//                 />
+//
+//                 <CardList monsters={filteredMonsters}/>
+//             </>
+//         )
+//     }
+// }
 
 export default App
