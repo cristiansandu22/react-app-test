378f8831fb8200f2d92b4d8579ebdc1c603ae791
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-23 14:53:09 +0200

    class removed
    CardList component is now a function component.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,3 @@
-import {Component, useState} from "react";
 import './card-list.styles.css'
 import Card from "../card/card.component.jsx";
 
@@ -14,21 +13,5 @@ const CardList = ({monsters}) => {
         </div>
     )
 }
-
-// class CardList extends Component {
-//     render() {
-//         const {monsters} = this.props
-//
-//         return (
-//             <div className='grid'>
-//                 {monsters.map(monster => {
-//                     return (
-//                        <Card key={monster.id} monster={monster} />
-//                     )
-//                 })}
-//             </div>
-//         )
-//     }
-// }
 
 export default CardList;
