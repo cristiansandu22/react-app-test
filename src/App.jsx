017fec46887a3f5e736a289b24604cd384ca7502
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-23 14:10:21 +0200

    Deleted h1

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,6 @@ const App = () => {
 
     return(
         <>
-            <h1>Hello</h1>
             <SearchBox
                 onChangeHandler={onChangeField}
                 inputPlaceholder='Search monsters'
