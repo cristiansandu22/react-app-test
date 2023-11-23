b42fc4ad8fc9208b11150a07e021270580ae90b3
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-23 14:56:40 +0200

    Card class component
    Card class component converted to functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,19 +1,10 @@
-import {Component} from "react";
-import './card.style.css'
 
-class Card extends Component {
-
-    render() {
-
-        //destructuring name from this.props.monster
-        const {name} = this.props.monster
-
-        return(
-            <div className='card-container'>
-                <h3>{name}</h3>
-            </div>
-        )
-    }
+const Card = ({name}) => {
+    return (
+        <div className='card-container'>
+            <h3>{name}</h3>
+        </div>
+    )
 }
 
 export default Card
