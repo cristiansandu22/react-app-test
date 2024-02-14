7469ec3667612c77d78bae72163bfc638b621eda
cristiansandu22 <cristian.sandu@welthee.com>
2024-02-14 18:05:39 +0200

    userSlice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,17 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cakeSlice from "../features/cake/cakeSlice.js";
 import iceCreamSlice from "../features/icecream/iceCreamSlice.js";
+import userSlice from "../features/user/userSlice.js";
 
 const cakeReducer = cakeSlice.reducer;
 const iceCreamReducer = iceCreamSlice.reducer;
+const userReducer = userSlice.reducer;
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        iceCream: iceCreamReducer
+        iceCream: iceCreamReducer,
+        user: userReducer,
     }
 })
 
--- src/test.js
+++ src/test.js
@@ -1,23 +1,28 @@
 import store from "./app/store.js";
 import cakeSlice from "./features/cake/cakeSlice.js";
 import iceCreamSlice from "./features/icecream/iceCreamSlice.js";
+import {fetchUsers} from "./features/user/userSlice.js";
 
 const cakeActions = cakeSlice.actions;
 const iceCreamActions = iceCreamSlice.actions;
 
 console.log('This is the initial state', store.getState());
 
+
+
 const unsubscribe = store.subscribe(() => {
     console.log('Updates state', store.getState())
 })
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(10));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+//
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(10));
 
-unsubscribe();
+// unsubscribe();
